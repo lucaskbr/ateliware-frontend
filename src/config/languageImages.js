20f5bee8085ae670ee91ab5817db89e042e94190
lucaskbr <lucas_klasa@hotmail.com>
2020-04-25 15:17:55 -0300

    chore: add config file

---
+++ src/config/languageImages.js
@@ -0,0 +1,33 @@
+import random from '../assets/random.png';
+import java from '../assets/java.png';
+import javascript from '../assets/javascript.png';
+import python from '../assets/python.png';
+import ruby from '../assets/ruby.png';
+import elixir from '../assets/elixir.png';
+
+export default [
+  {
+    name: 'random',
+    image: random,
+  },
+  {
+    name: 'java',
+    image: java,
+  },
+  {
+    name: 'javascript',
+    image: javascript,
+  },
+  {
+    name: 'python',
+    image: python,
+  },
+  {
+    name: 'ruby',
+    image: ruby,
+  },
+  {
+    name: 'elixir',
+    image: elixir,
+  },
+];
