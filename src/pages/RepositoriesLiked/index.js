7c973c3dfdd1587a49a12b599428fb6c5d7a4173
lucaskbr <lucas_klasa@hotmail.com>
2020-04-25 15:22:54 -0300

    feat: add repo liked page

---
+++ src/pages/RepositoriesLiked/index.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
+import { ToastContainer, toast } from 'react-toastify';
+import BarLoader from 'react-spinners/BarLoader';
+
+import api from '../../services/api';
+
+import openNewTab from '../../utils/openNewTab';
+
+import toastConfig from '../../config/toastCofig';
+
+import Repository from '../../components/Repository';
+
+import {
+  Container,
+  Title,
+  RepositoriesList,
+  Pagination,
+  PaginationButton,
+} from './styles';
+
+export default function RepositoriesLiked() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [repositories, setRepositories] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function fetchApi() {
+      try {
+        const response = await api.get(`/favorite-repositories?page=${page}`);
+        if (response.data.length === 0) {
+          toast.error(
+            'Não existe mais informações a serem exbidas',
+            toastConfig
+          );
+          setPage(page - 1);
+          return;
+        }
+
+        setRepositories(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        toast.error(
+          '😭 Ocorreu um erro, tente novamente mais tarde',
+          toastConfig
+        );
+      }
+    }
+
+    fetchApi();
+  }, [page]);
+
+  return (
+    <Container>
+      {isLoading ? (
+        <BarLoader width={300} color="#ef0044" />
+      ) : (
+        <>
+          <Title>
+            Repositories liked/
+            <span role="img" aria-label="glass">
+              😍
+            </span>
+          </Title>
+
+          <RepositoriesList>
+            {repositories.map((r, i) => (
+              <Repository
+                key={r.id}
+                item={r}
+                disableLike
+                onClick={() => openNewTab(r.url)}
+              />
+            ))}
+          </RepositoriesList>
+          <Pagination>
+            <PaginationButton
+              type="button"
+              disabled={page === 1}
+              allowed={page === 1}
+              onClick={() => setPage(page - 1)}
+            >
+              <AiOutlineLeft color="#000" size={20} />
+            </PaginationButton>
+            {page}
+            <PaginationButton type="button" onClick={() => setPage(page + 1)}>
+              <AiOutlineRight color="#000" size={20} />
+            </PaginationButton>
+          </Pagination>
+          <ToastContainer />
+        </>
+      )}
+    </Container>
+  );
+}
+/**
+ *
+ */
---
+++ src/pages/RepositoriesLiked/styles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 40px;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 900;
+  margin-top: 50px;
+`;
+
+export const RepositoriesList = styled.div`
+  margin-top: 50px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const Pagination = styled.div`
+  width: 100%;
+  max-width: 700px;
+  background: #f7f7f7;
+  color: #000;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 4px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  margin: 15px auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const PaginationButton = styled.button.attrs((props) => ({
+  type: 'button',
+}))`
+  background: #f7f7f7;
+  font-weight: 600;
+  border: 4px;
+  padding: 15px 30px;
+  border-radius: 0px;
+  cursor: ${(props) => props.allowed && 'not-allowed'};
+  &:hover {
+    background: #eae8e8;
+    transition: 0.2s;
+  }
+`;
