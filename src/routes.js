34e3e38c6a9866bd0cbeac18778fccd3129df494
lucaskbr <lucas_klasa@hotmail.com>
2020-04-25 15:23:50 -0300

    feat: add route repo liked

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Repositories from './pages/Repositories';
+import RepositoriesLiked from './pages/RepositoriesLiked';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/repositories/:language/:q" component={Repositories} />
+      <Route path="/repositories/likes" component={RepositoriesLiked} />
     </Switch>
   );
 }
