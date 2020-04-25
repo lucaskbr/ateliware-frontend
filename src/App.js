b2d931d7f852d4027c4aafe39d87b0e7b35c27ba
lucaskbr <lucas_klasa@hotmail.com>
2020-04-25 15:23:30 -0300

    feat: add header component global

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
+        <Header />
         <Routes />
       </BrowserRouter>
     </>
